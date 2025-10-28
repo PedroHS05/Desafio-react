944a3ea3b84ab362bc7a39b5ab65151620dc5174
PedroHS05 <pedro.scabelo2b2@gmail.com>
2025-10-28 12:03:45 -0300

    Primeiro commit - projeto React

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import ChallengComponent from './components/ChallengComponent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='div1'>
+          <h1>Essa é minha atividade em React</h1>
+        </div>
+        <ChallengComponent/>
       </header>
     </div>
   );
